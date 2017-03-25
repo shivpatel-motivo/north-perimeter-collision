6e9af08288d71133b774bb84725c6f88ac96f965
Shiv Patel <shivpatelssp@gmail.com>
2017-03-24 21:01:16 -0400

    converting to html

--- footer.php
+++
@@ -1,5 +0,0 @@
-</body>
-	<div id="footer">
-		<p>Copyright 2012 <a href="http://northperimetercollision.com/">North Perimeter Collision</a> &nbsp;&nbsp;|&nbsp;&nbsp; Website by <a href="http://motivosolutions.com/">Motivo</a></p>
-	</div>
-</html>
--- sidebar.php
+++
@@ -1,5 +0,0 @@
-				<h1>Contact Info</h1>
-				<div id="sidebarBox">
-				<p style="margin-top:0px;padding-top:0px;">(770) 458-4875<br><a href="mailto:northperimeter@aol.com">northperimeter@aol.com</a><br><br>3854 North Peachtree Road<br>Chamblee, GA. 30341<br>(<a href="http://goo.gl/maps/iXFD">Google Maps</a>)</p>
-				<p style="margin-bottom:0px;padding-bottom:0px;">Click <a href="../contact.php">here</a> for more information.</p>
-				</div>
